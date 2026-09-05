c3534916aa37b3a08ba5bff4c4d301835c8fded4
ewgdyfjwq <gravellambert@gmail.com>
2026-09-05 19:30:42 -0400

    Update python.py

--- python.py
+++ python.py
@@ -2,4 +2,4 @@ lenght = float(input('what the lenght:'))
 widght = float(input('what the widght: '))
 area = lenght * widght
 print(area)
-input('\nPress Enter to close...)
+input('\nPress Enter to close...')
